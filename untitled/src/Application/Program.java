93a1d7b158d11cbd0ef1edb96a00e239cc2bb565
= <bethaniafreitas@yahoo.com.br>
2023-02-01 12:51:16 -0300

    Classe position

--- untitled/src/Application/Program.java
+++ untitled/src/Application/Program.java
@@ -1,8 +1,11 @@
 package Application;
 
+import boardGame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("teste");
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 
 }
